0b33d89ee699ce383496fb8b0341969a1fb0bb7a
RostyslavTsarkov <rostyslav@thewhitelabelagency.com>
2024-02-27 18:45:06 +0200

    WP_Query for archive and taxonome pages was separated. Caption structure was changed for better.

--- inc/projects-slider.php
+++ inc/projects-slider.php
@@ -35,21 +35,33 @@ function featured_projects($atts)
     </script>
 
     <?php
-    $slider = new WP_Query([
-        'post_type' => 'project',
-        'tax_query' => array(
-            array (
-                'taxonomy' => 'project-type',
-                'field' => 'slug',
-                'terms' => $atts['term'],
-            )
-        ),
-        'order' => 'ASC',
-        'orderby' => 'menu_order',
-        'posts_per_page' => 5,
-        'meta_key' => 'is_featured_project',
-        'meta_value' => '1',
-    ]);
+    if ($atts['term'] == '') {
+        $slider = new WP_Query([
+            'post_type' => 'project',
+            'order' => 'ASC',
+            'orderby' => 'menu_order',
+            'posts_per_page' => 5,
+            'meta_key' => 'is_featured_project',
+            'meta_value' => '1',
+        ]);
+    } else {
+        $slider = new WP_Query([
+            'post_type' => 'project',
+            'tax_query' => array(
+                array (
+                    'taxonomy' => 'project-type',
+                    'field' => 'slug',
+                    'terms' => $atts['term'],
+                )
+            ),
+            'order' => 'ASC',
+            'orderby' => 'menu_order',
+            'posts_per_page' => 5,
+            'meta_key' => 'is_featured_project',
+            'meta_value' => '1',
+        ]);
+    }
+
     if ($slider->have_posts()) : ?>
     <div class="grid-container full">
         <div id="home-slider" class="slick-slider home-slider">
@@ -57,15 +69,26 @@ function featured_projects($atts)
                 $slider->the_post(); ?>
                 <div class="slick-slide home-slide">
                     <div class="home-slide__inner" <?php bg(esc_url(get_field('project_slide_images')[0]['url']), 'full_hd'); ?>>
-
                     </div>
                     <div class="home-slide__caption">
                         <div class="grid-x">
-                            <a class="slide__post-link cell grid-x align-justify align-middle"
-                               href="<?php the_permalink(); ?>">
-                                <h5 class="text-uppercase font-weight-300"><?php the_title(); ?></h5>
-                                <i class="arrow fa-solid fa-chevron-right"></i>
-                            </a>
+                            <div class="cell">
+                                <span class="slide__post-link grid-x align-justify align-middle" onclick="location='<?php the_permalink(); ?>'"  style="cursor: pointer">
+                                    <h5 class="small-6 text-uppercase font-weight-300"><?php the_title(); ?></h5>
+                                    <div class="small-6">
+                                        <div class="grid-y">
+                                            <div class="grid-x align-right font-size-150 roman-silver">
+                                                <?php if ($client = get_field('project_client')) {
+                                                    echo esc_html($client->name);
+                                                } ?>
+                                            </div>
+                                            <div class="grid-x align-right dark-gunmetal">
+                                                <?php echo get_the_term_list(get_the_ID(), 'project-type', '', ',', ''); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
