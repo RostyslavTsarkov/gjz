a4f39a1d736325f47a55060f5fb9489dc6c2ecd2
RostyslavTsarkov <rostyslav@thewhitelabelagency.com>
2024-03-01 18:17:38 +0200

    The way of getting terms was changed. Arrow was changed to stylish one.

--- templates/template-home.php
+++ templates/template-home.php
@@ -17,30 +17,24 @@ get_header(); ?>
             <h1 class="font-weight-900"><?php the_field('about_company_name'); ?></h1>
             <div class="font-size-500 font-weight-300 black-coral"><?php the_field('about_description'); ?></div>
         </div>
-        <div class="cell">
-            <div class="about__category-list grid-x grid-margin-x row-gap-20 align-justify medium-align-center">
-                <?php
-                $args = array(
-                    'taxonomy'   => 'project-type',
-                    'orderby'    => 'id',
-                    'order'      => 'ASC',
-                    'hide_empty' => false,
-                );
-                $terms = get_terms($args); ?>
-                <?php if (! empty($terms) && is_array($terms)) :
-                    foreach ($terms as $term) : ?>
+        <?php if ($terms = get_field('home_featured_categories')) : ?>
+            <div class="cell">
+                <div class="about__category-list grid-x grid-margin-x row-gap-20 align-justify medium-align-center">
+                    <?php foreach ($terms as $term) : ?>
                         <article class="cell large-auto category-block">
                             <a href="<?php echo esc_url(get_term_link($term)); ?>">
-                                <h6 class="text-lowercase font-size-350 font-weight-700"><?php echo $term->name; ?></h6>
+                                <h6 class="text-lowercase font-size-350 font-weight-700"><?php echo esc_html($term->name); ?></h6>
                             </a>
-                            <p class="category-block__description font-size-250 font-weight-300 black-coral"><?php echo $term->description; ?></p>
+                            <p class="category-block__description font-size-250 font-weight-300 black-coral"><?php echo esc_html($term->description); ?></p>
                             <a href="<?php echo esc_url(get_term_link($term)); ?>"
-                                class="font-size-300 fa-solid fa-chevron-right"></a>
+                               class="font-size-300">
+                                <div class="styled-arrow"></div>
+                            </a>
                         </article>
-                    <?php endforeach;
-                endif; ?>
+                    <?php endforeach; ?>
+                </div>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
 </section>
 <ar
