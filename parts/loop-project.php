d63ca501e520348a279f1041b3f7510f5c21c04f
RostyslavTsarkov <rostyslav@thewhitelabelagency.com>
2024-03-01 18:16:51 +0200

    Project image was wrapped to link. Styles for text was changed. Arrow was changed to stylish one.

--- parts/loop-project.php
+++ parts/loop-project.php
@@ -1,27 +1,34 @@
 <!-- BEGIN of Project -->
 <article id="post-<?php the_ID(); ?>" <?php post_class('project-block masonry-item preview preview--' . get_post_type()); ?>>
-    <div class="grid-y">
+    <div class="grid-y row-gap-15">
         <?php if ($gallery = get_field('project_slide_images')) : ?>
-            <div class="cell rel-content project-block__featured-img--container">
-                <?php echo wp_get_attachment_image($gallery[0]['id'], false, false, array('class' => 'project-block__featured-img')); ?>
-            </div>
-        <?php endif; ?>
-        <div class="cell">
-            <h6 class="preview__title">
-                <a href="<?php the_permalink(); ?>"
-                   title="<?php echo esc_attr(sprintf(__('Permalink to %s', 'fxy'), the_title_attribute('echo=0'))); ?>"
-                   rel="bookmark"><?php echo get_the_title() ?: __('No title', 'fxy'); ?>
-                </a>
-            </h6>
-            <p><?php echo get_the_terms(get_the_ID(), 'client')[0]->name; ?></p>
-        </div>
-        <div class="">
-            <a href="<?php the_permalink(); ?>"
-               title="<?php echo esc_attr(sprintf(__('Permalink to %s', 'fxy'), the_title_attribute('echo=0'))); ?>"
-               rel="bookmark">
-                details
-                <i class="fa-solid fa-chevron-right"></i>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                <div class="cell rel-content project-block__featured-img--container">
+                    <?php the_post_thumbnail('medium', array('class' => 'preview__thumb project-block__featured-img')); ?>
+                </div>
             </a>
+        <?php endif; ?>
+        <div class="cell project-block__body">
+            <div class="grid-y">
+                <h6 class="preview__title font-weight-600 font-size-250">
+                    <a href="<?php the_permalink(); ?>"
+                       title="<?php echo esc_attr(sprintf(__('Permalink to %s', 'fxy'), the_title_attribute('echo=0'))); ?>"
+                       rel="bookmark"><?php echo get_the_title() ?: __('No title', 'fxy'); ?>
+                    </a>
+                </h6>
+                <p class="roman-silver font-size-200">
+                    <?php if ($client = get_field('project_client')) {
+                        echo esc_html($client->name);
+                    } ?>
+                </p>
+                <a class="text-right font-size-250"
+                    href="<?php the_permalink(); ?>"
+                   title="<?php echo esc_attr(sprintf(__('Permalink to %s', 'fxy'), the_title_attribute('echo=0'))); ?>"
+                   rel="bookmark">
+                    <i>details</i>
+                    <div class="styled-arrow"></div>
+                </a>
+            </div>
         </div>
     </div>
 </article>
